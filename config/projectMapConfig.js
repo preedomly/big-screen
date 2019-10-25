74ea7664e2ed91acb091af4f91509f07d82b4df6
luyue <preedomly@163.com>
2019-10-25 15:20:40 +0800

    更新大屏信息的配置文件，设置图层keys

--- config/projectMapConfig.js
+++ config/projectMapConfig.js
@@ -1,21 +1,64 @@
-var projectMapConfig = {
-  // 按配置顺序加载
-  tileLayers: [
-    {
-      url: 'proxy6080/arcgis/rest/services/kqgpsdp/kqdp/MapServer',
-    }
+const projectMapConfig = {
+  OneMap: {
+    // 按配置顺序加载
+    tileLayers: [
+      {
+        url: 'proxy6080/arcgis/rest/services/kqgpsdp/kqdp/MapServer',
+      }
 
-  ],
-  vectorLayers: [
-    {
-      url: 'proxy6080/arcgis/rest/services/kqgps/kqdppszgx_sl/MapServer',
-    }
-  ],
-  // api: 'http://192.168.30.212:8085',
-  // api: 'http://192.168.8.29:8085/ecity_js_api1.0',
-  api: 'http://2140u809i6.imwork.net:10678/ecity_js_api1.0',
-  maxscale: '250000',
-  minscale: '25000',
-  width: 3700,
-  height: 1780,
+    ],
+    vectorLayers: [
+      {
+        url: 'proxy6080/arcgis/rest/services/kqgps/kqdppszgx_sl/MapServer',
+      }
+    ],
+    // api: 'http://192.168.30.212:8085',
+    // api: 'http://192.168.8.29:8085/ecity_js_api1.0',
+    api: 'http://2140u809i6.imwork.net:10678/ecity_js_api1.0',
+    maxscale: '250000',
+    minscale: '25000',
+    width: 3700,
+    height: 1780,
+  },
+  TwoMap: {
+    // 按配置顺序加载
+    tileLayers: [
+      {
+        url: 'proxy6080/arcgis/rest/services/kqgpsdp/kqdp/MapServer',
+      }
+
+    ],
+    vectorLayers: [
+      {
+        url: 'proxy6080/arcgis/rest/services/kqgps/kqdppszgx_sl/MapServer',
+      }
+    ],
+    // api: 'http://192.168.30.212:8085',
+    // api: 'http://192.168.8.29:8085/ecity_js_api1.0',
+    api: 'http://2140u809i6.imwork.net:10678/ecity_js_api1.0',
+    maxscale: '250000',
+    minscale: '25000',
+    width: 3700,
+    height: 1780,
+  },
+  thridMap: {
+    tileLayers: [
+      {
+        url: 'proxy6080/arcgis/rest/services/kqgpsdp/kqdp/MapServer',
+      }
+
+    ],
+    vectorLayers: [
+      {
+        url: 'proxy6080/arcgis/rest/services/kqgps/kqdppszgx_sl/MapServer',
+      }
+    ],
+    // api: 'http://192.168.30.212:8085',
+    // api: 'http://192.168.8.29:8085/ecity_js_api1.0',
+    api: 'http://2140u809i6.imwork.net:10678/ecity_js_api1.0',
+    maxscale: '250000',
+    minscale: '25000',
+    width: 3700,
+    height: 1780,
+  }
 }
