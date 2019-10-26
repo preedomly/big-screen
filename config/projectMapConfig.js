4122d33a09708aacf72838a37b4c9b5da3a079df
luyue <preedomly@163.com>
2019-10-26 17:23:46 +0800

    1、地图配置化，根据 projectMapConfig.js 文件里面的配置信息来配置地图 2、地图增加热力图、tip框内容优化、假数据接入， 3、增加泵站信息、智慧眼、终端的数据接入

--- config/projectMapConfig.js
+++ config/projectMapConfig.js
@@ -3,22 +3,31 @@ const projectMapConfig = {
     // 按配置顺序加载
     tileLayers: [
       {
+        id: "kqdp",
         url: 'proxy6080/arcgis/rest/services/kqgpsdp/kqdp/MapServer',
       }
-
     ],
     vectorLayers: [
       {
+        id: "kqdppszgx_sl",
         url: 'proxy6080/arcgis/rest/services/kqgps/kqdppszgx_sl/MapServer',
       }
     ],
-    // api: 'http://192.168.30.212:8085',
     // api: 'http://192.168.8.29:8085/ecity_js_api1.0',
     api: 'http://2140u809i6.imwork.net:10678/ecity_js_api1.0',
-    maxscale: '250000',
-    minscale: '25000',
-    width: 3700,
-    height: 1780,
+    maxscale: '500000',
+    minscale: '2000',
+    centerx: 53000, // 中心点X坐标
+    centery: 44000, // 中心点Y坐标
+    initlevel: 5,   // 初始化放大至
+    extent: {
+      xmin: 525098.7384641441,
+      xmax: 579295.0385234108,
+      ymin: 3285906.383879434,
+      ymax: 3353190.8538650405
+    },  // 全部大小范围
+    width: 3700,  // 地图大小
+    height: 1780, // 地图大小
   },
   TwoMap: {
     // 按配置顺序加载
@@ -33,12 +42,20 @@ const projectMapConfig = {
         url: 'proxy6080/arcgis/rest/services/kqgps/kqdppszgx_sl/MapServer',
       }
     ],
-    // api: 'http://192.168.30.212:8085',
     // api: 'http://192.168.8.29:8085/ecity_js_api1.0',
     api: 'http://2140u809i6.imwork.net:10678/ecity_js_api1.0',
-    maxscale: '250000',
-    minscale: '25000',
-    width: 3700,
+    maxscale: '500000',
+    minscale: '2000',
+    centerx: 53000,
+    centery: 44000,
+    initlevel: 5,
+    extent: {
+      xmin: 525098.7384641441,
+      xmax: 579295.0385234108,
+      ymin: 3285906.383879434,
+      ymax: 3353190.8538650405
+    },
+    width: 3500,
     height: 1780,
   },
   thridMap: {
@@ -53,11 +70,19 @@ const projectMapConfig = {
         url: 'proxy6080/arcgis/rest/services/kqgps/kqdppszgx_sl/MapServer',
       }
     ],
-    // api: 'http://192.168.30.212:8085',
     // api: 'http://192.168.8.29:8085/ecity_js_api1.0',
     api: 'http://2140u809i6.imwork.net:10678/ecity_js_api1.0',
-    maxscale: '250000',
-    minscale: '25000',
+    maxscale: '500000',
+    minscale: '2000',
+    centerx: 53000,
+    centery: 44000,
+    initlevel: 5,
+    extent: {
+      xmin: 525098.7384641441,
+      xmax: 579295.0385234108,
+      ymin: 3285906.383879434,
+      ymax: 3353190.8538650405
+    },
     width: 3700,
     height: 1780,
   }
