8bdd700c9ab46f221857ca6e73c09ef792e41fbf
Administrator <337882150@qq.com>
2019-11-01 17:44:49 +0800

    地图初试状态

--- config/layerId.js
+++ config/layerId.js
@@ -1,7 +1,7 @@
 const layersId = {
-  53: 'wisdomEye',
-  14: 'pumping',
-  54: 'terminal',
+  55: 'wisdomEye',
+  15: 'pumping',
+  56: 'terminal',
   // 3: 'vehicle',
   // 4: 'personnel',
   29: 'flow'
